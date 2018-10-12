e4cbd2f1b3acd2385aae66a5808a85de149896de
Jack Ling <j.lingyx@gmail.com>
2018-10-12 13:34:46 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,11 +59,11 @@ def runner
   welcome
   n = initial_round
   hit?(n)
-  display_card_total(n)
+  
   until n > 21 
   display_card_total(n)
-  new = hit?(n)
-  n = new + n
+  n = hit?(n)
+  display_card_total(n)
   end
 end_game(n)
 end
